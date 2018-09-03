a2e2a78f7cd42ac8d14e1f91224f3f7000c81fc6
tao <irrefuse@gmail.com>
2018-09-03 11:55:05 -0400

    plot

--- Section 4 simple linear regression/section_4.py
+++ Section 4 simple linear regression/section_4.py
@@ -7,18 +7,34 @@ Created on Thu Aug 30 23:14:47 2018
 """
 
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import pandas as pd
 
 dataset = pd.read_csv('Salary_Data.csv')
-X = dataset.iloc[:,0].values
+
+# X=dataset.iloc[:,0].values is wrong , because it gives a vector not a matrix with 1 comlum
+# It matters when fitting the regession model
+
+X = dataset.iloc[:,:-1].values
 y = dataset.iloc[:, 1].values
+
 
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size = 0.33, random_state=0)
 
-# Fitting simple linear regression model
-from sklearning.linear_model import LinearRegression
 
+X_train_reshape = X_train.reshape(-1,1)
+
+# Fitting simple linear regression model
+from sklearn.linear_model import LinearRegression
+
+#X_train.reshape(-1,1)
 regressor = LinearRegression()
-regressor.fit(X.train, y_train)
+regressor.fit(X_train, y_train)
+
+y_pred = regressor.predict(X_test)
+#visualize
+
+plt.scatter(X_train, y_train, color='red')
+plt.plot(X_train, regressor.predict(X_train), color='blue')
+plt.show()
