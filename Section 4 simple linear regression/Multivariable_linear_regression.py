86eeeb14fbb5f93bea2795ec4b6c51eb48cc3357
tao <irrefuse@gmail.com>
2018-09-11 00:10:34 -0400

    multi variable linear regression

---
+++ Section 4 simple linear regression/Multivariable_linear_regression.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep  9 22:19:10 2018
+
+@author: taojing
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv('50_Startups.csv')
+
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 4].values
+
+# encode categorical column
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelEncoder_X= LabelEncoder()
+X[:,3] = labelEncoder_X.fit_transform(X[:,3])
+oneHotEncoder = OneHotEncoder(categorical_features=[3])
+X = oneHotEncoder.fit_transform(X).toarray()
+
+#Avoiding dummpy variable trap
+X = X[: , 1:]
+
+# split into train and test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+#feature scaling , algorithum did it for us
+
+#fit multiple variable linear regression model
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train,y_train)
+
+#Predicting test_set result
+y_pred = regressor.predict(X_test)
+plt.scatter(y_test, y_pred)
+
+#Building the optimal model using backward elimination
+import statsmodels.formula.api as sm
+X = np.append(arr=np.ones((50,1)).astype(int), values = X, axis = 1)
+X_opt = X[:,[0,1,2,3,4,5]]
+# pay attention capitalization !
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+
+
+
+
+
+
+
